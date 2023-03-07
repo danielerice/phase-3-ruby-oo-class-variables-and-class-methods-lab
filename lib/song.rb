d2f58b47183b61cbe2d4ee8fe20618a4e020703c
Daniel Rice <danielrice5627@gmail.com>
2023-03-07 12:38:38 -0800

    commit tree b51b0248d8ef5b6206b73075a19052b1111a88ed

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    attr_accessor :name, :artist, :genre
+   
+    def initialize name, artist, genre
+        @@count += 1
+        @genre = genre
+        @name = name
+        @artist = artist
+
+        @@genres << genre
+        @@artists << artist
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+
+
+end
